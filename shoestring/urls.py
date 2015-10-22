9ad10e0e227246a1146f490993ce38d73cbac1ef
topdeveloper0729 <topdev0729@gmail.com>
2015-10-22 22:59:47 +1100

    Add cart urls

--- shoestring/urls.py
+++ shoestring/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url, include
 
 
 urlpatterns = [
+    url(r'^cart/',
+        include('shoestring.cart.urls', namespace='cart')),
     url(r'^accounts/',
-        include('shoestring.accounts.urls', namespace='accounts')
-        ),
+        include('shoestring.accounts.urls', namespace='accounts')),
     url(r'^products/',
-        include('shoestring.products.urls', namespace='products')
-        ),
+        include('shoestring.products.urls', namespace='products')),
 ]
