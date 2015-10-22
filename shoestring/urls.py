950b5d24245c1bbaae2762f6c59c94bc1ff85ee7
topdeveloper0729 <topdev0729@gmail.com>
2015-10-22 22:37:20 +1100

    Add shoestring urls

---
+++ shoestring/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+
+
+urlpatterns = [
+    url(r'^accounts/',
+        include('shoestring.accounts.urls', namespace='accounts')
+        ),
+    # url(r'^products/', include('shoestring.products.urls', namespace='products')),
+]
