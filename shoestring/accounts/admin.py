945632ae4cf6c6a072eaf1a659fec6c546aedd56
topdeveloper0729 <topdev0729@gmail.com>
2015-10-22 21:29:21 +1100

    Fix admin for User

--- shoestring/accounts/admin.py
+++ shoestring/accounts/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from . import models
+
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'is_active', 'is_staff',)
+    list_filter = ('is_active', 'is_staff',)
+    ordering = ('email',)
 
 admin.site.register(models.User, UserAdmin)
