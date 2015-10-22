5786b491b80d7ed0c8b8ae5703407fb5d4f91a85
topdeveloper0729 <topdev0729@gmail.com>
2015-10-22 22:17:52 +1100

    More UserAdmin tweaks

--- shoestring/accounts/admin.py
+++ shoestring/accounts/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 from . import models
 
 
 class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'is_active', 'is_staff',)
-    list_filter = ('is_active', 'is_staff',)
     ordering = ('email',)
+    search_fields = ('email',)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        (_('Permissions'), {
+            'fields': (
+                ('is_active', 'is_staff', 'is_superuser',),
+                'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+
 
 admin.site.register(models.User, UserAdmin)
