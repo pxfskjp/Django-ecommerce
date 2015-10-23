9305a0b68417f9403b329a96c70e55de2ff10c9a
topdeveloper0729 <topdev0729@gmail.com>
2015-10-23 18:12:28 +1100

    Add Price filter to Cart widget

--- shoestring/static/shoestring/js/cart.js
+++ shoestring/static/shoestring/js/cart.js
@@ -1,6 +1,10 @@
 
 var Cart = Ractive.extend({
     template: '#cart',
+    isolated: true,
+    data: {
+        price: function (item) { return item.qty * item.amount; }
+    },
     load: function () {
         rpc_call(SS.url.cart, 'content', {}, function (data) {
             this.set(data);
