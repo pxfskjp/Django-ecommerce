092fc8ef584d5e460eb05c2a237eac3b0f0ae393
topdeveloper0729 <topdev0729@gmail.com>
2015-10-25 13:32:18 +1100

    Continue cart component

--- shoestring/static/shoestring/js/cart.js
+++ shoestring/static/shoestring/js/cart.js
@@ -1,13 +1,18 @@
 
+function dollar_fmt (value) { return value.toFixed(2); }
+
 var Cart = Ractive.extend({
     template: '#cart',
     isolated: true,
     data: {
-        price: function (item) { return item.qty * item.amount; }
+        total: function (item) { return dollar_fmt(item.qty * item.price); },
+        dollar_fmt: dollar_fmt
     },
+    update: function (data) { this.set('cart', data); },
     load: function () {
-        rpc_call(SS.url.cart, 'content', {}, function (data) {
-            this.set(data);
-        }.bind(this));
+        rpc_call(SS.url.cart, 'content', {}, this.update.bind(this));
+    },
+    add: function (sku, qty) {
+        rpc_call(SS.url.cart, 'add', {sku: sku, qty: qty || 1}, this.update.bind(this));
     }
 });
