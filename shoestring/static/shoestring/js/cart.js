62076067fc8edcc911835e1e7c42fbd28a17bff4
topdeveloper0729 <topdev0729@gmail.com>
2015-11-07 23:13:30 +1100

    Make Cart container object

--- shoestring/static/shoestring/js/cart.js
+++ shoestring/static/shoestring/js/cart.js
@@ -1,46 +1,59 @@
 
 function dollar_fmt (value) { return new Number(value).toFixed(2); }
 
-var Cart = Ractive.extend({
+// Container for cart details
+function Cart() {
+    this.total = 0;
+    this.items = [];
+}
+Cart.prototype.update = function (data) {
+    this.total = data.total;
+    this.items = data.items;
+    // this is brutal
+    ractive.update();
+};
+Cart.prototype.load = function () {
+    rpc_call(SS.url.cart, 'content', {}, this.update.bind(this));
+};
+Cart.prototype.clear = function () {
+    rpc_call(SS.url.cart, 'clear', {}, this.update.bind(this));
+};
+Cart.prototype.add = function (sku, qty) {
+    rpc_call(SS.url.cart, 'add', {sku: sku, qty: qty || 1}, this.update.bind(this));
+};
+Cart.prototype.remove = function (sku) {
+    rpc_call(SS.url.cart, 'quantity', {sku: sku, qty: 0}, this.update.bind(this));
+};
+Cart.prototype.quantity = function (sku, qty) {
+    rpc_call(SS.url.cart, 'quantity', {sku: sku, qty: qty}, this.update.bind(this));
+};
+
+// Ractive for full cart
+var FullCart = Ractive.extend({
     template: '#cart',
     isolated: true,
+    magic: true,
     data: {
         total: function (item) { return dollar_fmt(item.qty * item.price); },
         dollar_fmt: dollar_fmt
     },
-    update: function (data) { this.set('cart', data); },
-    load: function () {
-        rpc_call(SS.url.cart, 'content', {}, this.update.bind(this));
-    },
-    clear: function () {
-        rpc_call(SS.url.cart, 'clear', {}, this.update.bind(this));
-    },
-    add: function (sku, qty) {
-        rpc_call(SS.url.cart, 'add', {sku: sku, qty: qty || 1}, this.update.bind(this));
-    },
-    remove: function (sku) {
-        rpc_call(SS.url.cart, 'quantity', {sku: sku, qty: 0}, this.update.bind(this));
-    },
-    quantity: function (sku, qty) {
-        rpc_call(SS.url.cart, 'quantity', {sku: sku, qty: qty}, this.update.bind(this));
-    },
     oninit: function () {
         this.on({
             'incCartItem': function (ev) {
-                this.quantity(ev.context.sku, ev.context.qty + 1);
+                this.cart.quantity(ev.context.sku, ev.context.qty + 1);
             }.bind(this),
             'decCartItem': function (ev) {
                 if(ev.context.qty > 1) {
-                    this.quantity(ev.context.sku, ev.context.qty - 1);
+                    this.cart.quantity(ev.context.sku, ev.context.qty - 1);
                 } else {
-                    this.remove(ev.context.sku);
+                    this.cart.remove(ev.context.sku);
                 }
             }.bind(this),
             'clearCart': function (ev) {
-                this.clear();
+                this.cart.clear();
             }.bind(this)
         });
     }
 });
 
-var MiniCart = Cart.extend({template: '#mini-cart'});
+var MiniCart = FullCart.extend({template: '#mini-cart'});
