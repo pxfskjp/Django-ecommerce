57d627b584a08a4927711a424d6f95859b313c3e
topdeveloper0729 <topdev0729@gmail.com>
2015-11-18 18:28:13 +1100

    Add fallback image for products

--- shoestring/static/shoestring/js/products.js
+++ shoestring/static/shoestring/js/products.js
@@ -1,6 +1,12 @@
 
 var ProductList = Ractive.extend({
     template: '#product-list',
+    data: {
+        image: function (src) {
+            src = src || 'products/default.png';
+            return 'products/product-list/' + src;
+        }
+    },
     computed: {
         sku_map: function () {
             var o = {};
