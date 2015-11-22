bbff95e0a71caddf6fe985014f11dfa6f4fc504e
topdeveloper0729 <topdev0729@gmail.com>
2015-11-22 12:44:10 +1100

    Make filter-list update state on click

--- shoestring/static/shoestring/js/filter-list.js
+++ shoestring/static/shoestring/js/filter-list.js
@@ -1,4 +1,9 @@
 var FilterList = Ractive.extend({
     template: '#filter-list',
-    magic: true
+    magic: true,
+    oninit: function () {
+        this.on('toggleFilter', function (event) {
+            this.set(event.keypath + ".active", !event.context.active);
+        }.bind(this));
+    }
 });
