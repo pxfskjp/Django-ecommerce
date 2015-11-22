7f8ec983469db36689b889774849318d2b566a86
topdeveloper0729 <topdev0729@gmail.com>
2015-11-22 12:43:40 +1100

    Make parse_qs more useful

--- shoestring/static/shoestring/js/utils.js
+++ shoestring/static/shoestring/js/utils.js
@@ -1,7 +1,7 @@
 
-parse_qs = function () {
+parse_qs = function (qs) {
     var data = {};
-    document.location.search.replace(/^\?/, '').split('&').forEach(function(term) {
+    qs.replace(/^\?/, '').split('&').forEach(function(term) {
         term = term.split('=', 2);
         (term[0] in data) || (data[term[0]] = []);
         data[term[0]].push(unescape(term[1]));
