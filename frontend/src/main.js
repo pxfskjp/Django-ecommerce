2892b18e5f15c439ceb46ffd5d589bd419e23c04
topdeveloper0729 <topdev0729@gmail.com>
2018-03-31 17:40:55 +1100

    Make filters work

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,29 @@ import { Store } from 'svelte/store.js';
 import { getCookie } from './cookies.js';
 import request from './request.js';
 
-const store = new Store({
+import './global.scss';
+
+class ShoestringStore extends Store {
+	toggleFilter(key, name) {
+		let items = this.get(key);
+		items.forEach(item => (item.name == name) && (item.active = !item.active));
+		let update = {}
+		update[key] = items;
+		this.set(update);
+		this.loadProducts();
+	}
+
+	loadProducts() {
+		request.json(SS.url.product, {
+			data: {
+				tag: this.get('tags').filter(tag => tag.active).map(tag => tag.name),
+				brand: this.get('brands').filter(brand => brand.active).map(brand => brand.name)
+			}
+		})
+		.then(data => this.set(data))
+	}
+}
+const store = new ShoestringStore({
 	products: [],
 	tags: [],
 	brands: [],
