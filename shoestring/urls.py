25285ae6f3d12b3704f5d3f297a3001be18ea1da
topdeveloper0729 <topdev0729@gmail.com>
2018-03-31 20:17:39 +1100

    Add media prefix to SS.urls

--- shoestring/urls.py
+++ shoestring/urls.py
@@ -1,11 +1,16 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
 
+
 urlpatterns = [
     url(r'^$',
         ensure_csrf_cookie(render),
-        {'template_name': 'shoestring/index.html'},
+        {
+            'template_name': 'shoestring/index.html',
+            'context': {'MEDIA_URL': settings.MEDIA_URL},
+        },
         name='index'),
     url(r'^cart/',
         include(('shoestring.cart.urls', 'shoestring'), namespace='cart')
