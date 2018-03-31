aa3a537323be8741b4141eda662c73de5a7a4c07
topdeveloper0729 <topdev0729@gmail.com>
2018-03-31 18:00:41 +1100

    Remove old components

--- shoestring/static/shoestring/js/filter-list.js
+++
@@ -1,9 +0,0 @@
-var FilterList = Ractive.extend({
-    template: '#filter-list',
-    magic: true,
-    oninit: function () {
-        this.on('toggleFilter', function (event) {
-            this.set(event.keypath + ".active", !event.context.active);
-        }.bind(this));
-    }
-});
