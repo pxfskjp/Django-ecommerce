69fcd4c41f0d40801ae0031a70775cbf81c9c53f
topdeveloper0729 <topdev0729@gmail.com>
2018-04-01 15:06:43 +1000

    Move common functions to utils

---
+++ frontend/src/utils.js
@@ -0,0 +1,9 @@
+
+let image_url = (alias, url) => { return SS.url.product + alias + '/' + url; }
+
+let dollarFormat = (value) => new Number(value).toFixed(2);
+
+export default {
+    dollarFormat,
+    imageUrl
+}
