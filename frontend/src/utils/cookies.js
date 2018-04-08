bee81cf6d66f08958e0aed0cca37cee776c23a69
topdeveloper0729 <topdev0729@gmail.com>
2018-04-08 13:08:43 +1000

    Spruce up the cookies code

--- frontend/src/utils/cookies.js
+++ frontend/src/utils/cookies.js
@@ -1,13 +1,12 @@
 export function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';').map(function (c) {
-            return c.trim().match(/(\w+)=(.*)/);
-        }).forEach(function (m) {
+
+    (document.cookie || '')
+        .split(';')
+        .map(c => c.trim().match(/(\w+)=(.*)/))
+        .forEach(m => {
             if(m !== undefined && m[1] == name) {
-                cookieValue = decodeURIComponent(m[2]);
+                return decodeURIComponent(m[2]);
             }
         });
-    }
-    return cookieValue;
+    return undefined;
 }
